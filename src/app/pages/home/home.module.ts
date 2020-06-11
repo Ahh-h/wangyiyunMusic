68835c66688a9646122091a7c5193575e258e0c7
charles <630705895@qq.com>
2020-06-11 21:37:34 +0800

    代码格式修改

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -4,7 +4,8 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { ShareModule } from 'src/app/share/share.module';
 
-
+// 用来管理所有的页面模块，只用把share模块引用进来就行了
+// 每个页面都能用到shareModule所有的模块，不用每个页面都去单独引入这些东西
 @NgModule({
   declarations: [HomeComponent],
   imports: [
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
-import { ShareModule } from '../share/share.module';
 import { HomeModule } from './home/home.module';
 
 
-// 用来管理所有的页面模块，只用把share模块引用进来就行了
-// 每个页面都能用到shareModule所有的模块，不用每个页面都去单独引入这些东西
+
 @NgModule({
   declarations: [],
   imports: [
@@ -12,7 +10,6 @@ import { HomeModule } from './home/home.module';
   ],
   exports: [
     HomeModule
-
   ]
 })
 export class PagesModule { }
